a1eeb411647a8f35d56c1d56bb6003219dc2987a
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-02-15 23:56:40 +0100

    overview page

---
+++ components/overview.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from "react"
+import { useDisconnect } from "@thirdweb-dev/react";
+import {
+  MediaRenderer,
+  useContract,
+  useContractMetadata,
+  Web3Button,
+  useUnclaimedNFTs,
+  useNFTs
+} from "@thirdweb-dev/react";
+
+const Overview = () => {
+
+  const disconnectWallet = useDisconnect(); // Hook to disconnect from the connected wallet.
+  const { contract: nftDrop } = useContract("0x4847482bb4E3c108aF1f7b6f05499C2D4246fE74");
+  const { data: contractMetadata, isLoading } = useContractMetadata(nftDrop);
+  const { data: nfts, isLoading2 } = useNFTs(nftDrop, {
+    start: 0,
+    count: 20,
+  });
+
+  
+
+  if (isLoading && isLoading2) {
+    return <div>Loading...</div>;
+  } else {
+    // const unclaimedNFTCount =  nftDrop.totalUnclaimedSupply();
+    console.log(nfts)
+  }
+
+  return (
+    <div className="overview">
+      <div onClick={() => disconnectWallet()}>
+        Disconnect Wallet
+      </div>
+      {nfts.map((item, i) => {
+        return(
+          <MediaRenderer
+            src={item.metadata.image}
+            alt={item.metadata.name}
+            style={{
+              width: "200px",
+            }}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default Overview
--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,52 @@
 import React, {useEffect, useState} from "react"
+import { useAddress } from "@thirdweb-dev/react";
+import { useMagic } from "@thirdweb-dev/react/evm/connectors/magic";
+import {
+  useContract,
+  useContractMetadata,
+} from "@thirdweb-dev/react";
+import Overview from "../components/overview";
 
 const Index = () => {
 
+  const address = useAddress(); // Hook to grab the currently connected user's address.
+  const connectWithMagic = useMagic(); // Hook to connect with Magic Link.
+
+  const [email, setEmail] = useState(""); // State to hold the email address the user entered.
+
+  const { contract: nftDrop } = useContract("0x4847482bb4E3c108aF1f7b6f05499C2D4246fE74");
+  const { data: contractMetadata, isLoading } = useContractMetadata(nftDrop);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="floating-nfts">
-      <div className="gradient"></div>
-      <video id="bgVideo" preload="true" autoPlay loop muted>
-        <source src="/nfts.mp4" type="video/mp4" /> 
-      </video>
-      <div className="step1">
-        <h1>START <br/>JOURNEY</h1>
-        <p>Fill in your email to create a wallet to collect your digital artwork:</p>
-        <input
-          type="email"
-          placeholder="Your Email Address"
-        />
-        <div className="login-link">
-          <img src="/triangle.svg"/>
+    <div className="main">
+      {address ? 
+        <div>
+          <Overview/>
         </div>
-      </div>
+      : 
+        <div className="floating-nfts">
+          <div className="gradient"></div>
+          <video id="bgVideo" preload="true" autoPlay loop muted>
+            <source src="/nfts.mp4" type="video/mp4" /> 
+          </video>
+          <div className="step1">
+            <h1>START <br/>JOURNEY</h1>
+            <p>Fill in your email to create a wallet to collect your digital artwork:</p>
+            <input
+              type="email"
+              placeholder="Your Email Address"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <div className="login-link" onClick={() => {connectWithMagic({ email });}}>
+              <img src="/triangle.svg"/>
+            </div>
+          </div>
+        </div>
+      }
     </div>
   )
 }
