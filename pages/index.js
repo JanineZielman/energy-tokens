0edf2ed13d0fac0199f98edad170f64efefef1db
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-02-15 23:19:42 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -4,9 +4,21 @@ const Index = () => {
 
   return (
     <div className="floating-nfts">
+      <div className="gradient"></div>
       <video id="bgVideo" preload="true" autoPlay loop muted>
         <source src="/nfts.mp4" type="video/mp4" /> 
-    </video>
+      </video>
+      <div className="step1">
+        <h1>START <br/>JOURNEY</h1>
+        <p>Fill in your email to create a wallet to collect your digital artwork:</p>
+        <input
+          type="email"
+          placeholder="Your Email Address"
+        />
+        <div className="login-link">
+          <img src="/triangle.svg"/>
+        </div>
+      </div>
     </div>
   )
 }
