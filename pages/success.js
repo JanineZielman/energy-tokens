1a129a77abe202647f75d1b1cd15cb6432e369c2
Janine Zielman <janinezielman@Janines-MacBook-Pro.local>
2023-02-21 22:16:43 +0100

    small

--- pages/success.js
+++ pages/success.js
@@ -6,7 +6,7 @@ const Index = () => {
 
   return (
     <div className="main success-page">
-      <Link href="/"><a className="back">Go back to your gallery</a></Link>
+      <Link href="/" className="back">Go back to your gallery</Link>
       <h2>The match reveal will take place on Sunday 26th at 11 AM CET. Come back and explore your gallery to see if the match was successful</h2>
     </div>
   )
